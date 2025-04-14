19e5972d52df9d99dbcc058c7b59527348f631f6
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-04-14 13:38:20 +0300

    Removed NoteListWidget and implemented scroll-synced sliver lists

--- lib/screens/feed_page.dart
+++ lib/screens/feed_page.dart
@@ -1,102 +1,166 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:qiqstr/widgets/note_list_widget.dart';
-import 'package:qiqstr/services/qiqstr_service.dart';
-import 'package:qiqstr/widgets/sidebar_widget.dart';
+import 'package:qiqstr/models/note_model.dart';
 import 'package:qiqstr/models/user_model.dart';
 import 'package:qiqstr/screens/share_note.dart';
+import 'package:qiqstr/services/qiqstr_service.dart';
+import 'package:qiqstr/widgets/note_widget.dart';
+import 'package:qiqstr/widgets/sidebar_widget.dart';
 
 class FeedPage extends StatefulWidget {
   final String npub;
-
   const FeedPage({super.key, required this.npub});
 
   @override
-  _FeedPageState createState() => _FeedPageState();
+  State<FeedPage> createState() => _FeedPageState();
 }
 
 class _FeedPageState extends State<FeedPage> {
-  UserModel? user;
-  late DataService dataService;
-  bool isLoading = true;
-  String? errorMessage;
+  final ScrollController _scrollController = ScrollController();
+
+  late final DataService _dataService;
+  final SplayTreeSet<NoteModel> _notes = SplayTreeSet(_compareNotes);
+
+  bool _isLoading = true;
+  bool _isLoadingOlder = false;
+  bool _showFab = true;
+  double _lastOffset = 0;
+  UserModel? _user;
+
+  static int _compareNotes(NoteModel a, NoteModel b) {
+    final aTime = a.isRepost ? (a.repostTimestamp ?? a.timestamp) : a.timestamp;
+    final bTime = b.isRepost ? (b.repostTimestamp ?? b.timestamp) : b.timestamp;
+    final res = bTime.compareTo(aTime);
+    return res == 0 ? a.id.compareTo(b.id) : res;
+  }
 
   @override
   void initState() {
     super.initState();
-    dataService = DataService(npub: widget.npub, dataType: DataType.Feed);
-    _loadUserProfile();
+    _dataService = DataService(
+      npub: widget.npub,
+      dataType: DataType.Feed,
+      onNewNote: _onNewNote,
+    );
+    _scrollController.addListener(_onScroll);
+    _initialize();
   }
 
-  Future<void> _loadUserProfile() async {
-    try {
-      await dataService.initialize();
-      final profileData = await dataService.getCachedUserProfile(widget.npub);
-      setState(() {
-        user = UserModel.fromCachedProfile(widget.npub, profileData);
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        errorMessage = 'An error occurred while loading the profile.';
-        isLoading = false;
-      });
+  void _onScroll() {
+    final offset = _scrollController.offset;
+    if (offset > _lastOffset + 20 && _showFab) {
+      setState(() => _showFab = false);
+    } else if (offset < _lastOffset - 20 && !_showFab) {
+      setState(() => _showFab = true);
+    }
+    _lastOffset = offset;
+
+    if (!_isLoadingOlder &&
+        offset >= _scrollController.position.maxScrollExtent - 200) {
+      _loadOlderNotes();
     }
   }
 
-  void _navigateToShareNotePage() {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => ShareNotePage(dataService: dataService),
-      ),
+  Future<void> _initialize() async {
+    await _dataService.initialize();
+    final profile = await _dataService.getCachedUserProfile(widget.npub);
+    _user = UserModel.fromCachedProfile(widget.npub, profile);
+
+    await _dataService.loadNotesFromCache((cached) {
+      _notes.addAll(cached);
+    });
+
+    await _dataService.initializeConnections();
+    setState(() => _isLoading = false);
+  }
+
+  Future<void> _loadOlderNotes() async {
+    setState(() => _isLoadingOlder = true);
+    await _dataService.fetchOlderNotes(
+      await _dataService.getFollowingList(widget.npub),
+      (note) => _notes.add(note),
     );
+    setState(() => _isLoadingOlder = false);
+  }
+
+  void _onNewNote(NoteModel note) {
+    if (_notes.add(note)) setState(() {});
+  }
+
+  void _goToShareNote() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (_) => ShareNotePage(dataService: _dataService)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _dataService.closeConnections();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: SidebarWidget(user: user),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _navigateToShareNotePage,
-        backgroundColor: Colors.white,
-        icon: SvgPicture.asset(
-          'assets/new_post_button.svg',
-          color: Colors.black,
-          width: 20.0,
-          height: 20.0,
-          fit: BoxFit.contain,
-        ),
-        label: const Text(
-          'New',
-          style: TextStyle(color: Colors.black),
+      drawer: SidebarWidget(user: _user),
+      floatingActionButton: AnimatedSlide(
+        offset: _showFab ? Offset.zero : const Offset(0, 2),
+        duration: const Duration(milliseconds: 300),
+        child: AnimatedOpacity(
+          opacity: _showFab ? 1 : 0,
+          duration: const Duration(milliseconds: 300),
+          child: FloatingActionButton.extended(
+            onPressed: _goToShareNote,
+            backgroundColor: Colors.white,
+            icon: SvgPicture.asset(
+              'assets/new_post_button.svg',
+              color: Colors.black,
+              width: 20,
+              height: 20,
+            ),
+            label: const Text('New', style: TextStyle(color: Colors.black)),
+          ),
         ),
       ),
-      body: isLoading
+      body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : errorMessage != null
-              ? Center(child: Text(errorMessage!))
-              : NestedScrollView(
-                  headerSliverBuilder:
-                      (BuildContext context, bool innerBoxIsScrolled) {
-                    return [
-                      SliverAppBar(
-                        title: Image.asset(
-                          'assets/main_icon.png',
-                          height: 35.0,
-                          fit: BoxFit.contain,
-                        ),
-                        pinned: false,
-                        snap: false,
-                        backgroundColor: Colors.black,
-                        iconTheme: const IconThemeData(color: Colors.white),
-                      ),
-                    ];
-                  },
-                  body: NoteListWidget(
-                    npub: widget.npub,
-                    dataType: DataType.Feed,
+          : CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: Colors.black,
+                  floating: true,
+                  snap: true,
+                  pinned: false,
+                  title: Image.asset('assets/main_icon.png', height: 35),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index == _notes.length && _isLoadingOlder) {
+                        return const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Center(child: CircularProgressIndicator()),
+                        );
+                      }
+                      final note = _notes.elementAt(index);
+                      return NoteWidget(
+                        note: note,
+                        dataService: _dataService,
+                        reactionCount: 0,
+                        replyCount: 0,
+                        repostCount: 0,
+                      );
+                    },
+                    childCount: _notes.length + (_isLoadingOlder ? 1 : 0),
                   ),
                 ),
+              ],
+            ),
     );
   }
 }
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,30 +1,116 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
-import 'package:qiqstr/widgets/note_list_widget.dart';
-import 'package:qiqstr/services/qiqstr_service.dart';
+import 'package:qiqstr/models/note_model.dart';
 import 'package:qiqstr/models/user_model.dart';
+import 'package:qiqstr/services/qiqstr_service.dart';
+import 'package:qiqstr/widgets/note_widget.dart';
 import 'package:qiqstr/widgets/profile_info_widget.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final UserModel user;
 
   const ProfilePage({super.key, required this.user});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final ScrollController _scrollController = ScrollController();
+  final SplayTreeSet<NoteModel> _notes = SplayTreeSet(_compareNotes);
+  late final DataService _dataService;
+
+  bool _isLoading = true;
+  bool _isLoadingOlder = false;
+
+  static int _compareNotes(NoteModel a, NoteModel b) {
+    final aTime = a.isRepost ? (a.repostTimestamp ?? a.timestamp) : a.timestamp;
+    final bTime = b.isRepost ? (b.repostTimestamp ?? b.timestamp) : b.timestamp;
+    final res = bTime.compareTo(aTime);
+    return res == 0 ? a.id.compareTo(b.id) : res;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _dataService = DataService(
+      npub: widget.user.npub,
+      dataType: DataType.Profile,
+      onNewNote: _onNewNote,
+    );
+    _scrollController.addListener(_onScroll);
+    _initialize();
+  }
+
+  void _onScroll() {
+    if (!_isLoadingOlder &&
+        _scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - 200) {
+      _loadOlderNotes();
+    }
+  }
+
+  Future<void> _initialize() async {
+    await _dataService.initialize();
+    await _dataService.loadNotesFromCache((cached) => _notes.addAll(cached));
+    await _dataService.initializeConnections();
+    setState(() => _isLoading = false);
+  }
+
+  Future<void> _loadOlderNotes() async {
+    setState(() => _isLoadingOlder = true);
+    await _dataService.fetchOlderNotes([widget.user.npub], (note) {
+      _notes.add(note);
+    });
+    setState(() => _isLoadingOlder = false);
+  }
+
+  void _onNewNote(NoteModel note) {
+    if (_notes.add(note)) setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _dataService.closeConnections();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverToBoxAdapter(
-              child: ProfileInfoWidget(user: user),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                SliverToBoxAdapter(
+                  child: ProfileInfoWidget(user: widget.user),
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index == _notes.length && _isLoadingOlder) {
+                        return const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Center(child: CircularProgressIndicator()),
+                        );
+                      }
+                      final note = _notes.elementAt(index);
+                      return NoteWidget(
+                        note: note,
+                        dataService: _dataService,
+                        reactionCount: 0,
+                        replyCount: 0,
+                        repostCount: 0,
+                      );
+                    },
+                    childCount: _notes.length + (_isLoadingOlder ? 1 : 0),
+                  ),
+                ),
+              ],
             ),
-          ];
-        },
-        body: NoteListWidget(
-          npub: user.npub,
-          dataType: DataType.Profile,
-        ),
-      ),
     );
   }
 }
