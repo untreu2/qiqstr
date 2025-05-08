f15a51e3a1995916db7de54660fb06666ab716ac
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-05-08 11:40:03 +0300

    Added follow/unfollow button to ProfileInfoWidget

--- lib/widgets/profile_info_widget.dart
+++ lib/widgets/profile_info_widget.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:qiqstr/models/user_model.dart';
 import 'package:qiqstr/utils/verify_nip05.dart';
+import 'package:qiqstr/services/data_service.dart';
+import 'package:hive/hive.dart';
+import 'package:qiqstr/models/following_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ProfileInfoWidget extends StatefulWidget {
@@ -15,12 +19,18 @@ class ProfileInfoWidget extends StatefulWidget {
 }
 
 class _ProfileInfoWidgetState extends State<ProfileInfoWidget> {
+  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
   bool? _isVerified;
+  bool? _isFollowing;
+  String? _currentUserNpub;
+  late Box<FollowingModel> _followingBox;
+  DataService? _dataService;
 
   @override
   void initState() {
     super.initState();
     _verifyIfNeeded();
+    _initFollowStatus();
   }
 
   Future<void> _verifyIfNeeded() async {
@@ -30,6 +40,43 @@ class _ProfileInfoWidgetState extends State<ProfileInfoWidget> {
       final result = await verifyNip05(nip05, pubkey);
       setState(() {
         _isVerified = result;
+      });
+    }
+  }
+
+  Future<void> _initFollowStatus() async {
+    _currentUserNpub = await _secureStorage.read(key: 'npub');
+    if (_currentUserNpub == null || _currentUserNpub == widget.user.npub)
+      return;
+
+    _followingBox = await Hive.openBox<FollowingModel>('followingBox');
+    final model = _followingBox.get('following_$_currentUserNpub');
+    final isFollowing = model?.pubkeys.contains(widget.user.npub) ?? false;
+    setState(() {
+      _isFollowing = isFollowing;
+    });
+
+    _dataService =
+        DataService(npub: _currentUserNpub!, dataType: DataType.Profile);
+    await _dataService!.initialize();
+  }
+
+  Future<void> _toggleFollow() async {
+    if (_currentUserNpub == null || _dataService == null) return;
+
+    setState(() {
+      _isFollowing = !_isFollowing!;
+    });
+
+    try {
+      if (_isFollowing!) {
+        await _dataService!.sendFollow(widget.user.npub);
+      } else {
+        await _dataService!.sendUnfollow(widget.user.npub);
+      }
+    } catch (e) {
+      setState(() {
+        _isFollowing = !_isFollowing!;
       });
     }
   }
@@ -61,12 +108,12 @@ class _ProfileInfoWidgetState extends State<ProfileInfoWidget> {
             width: screenWidth,
             height: 130,
             fit: BoxFit.cover,
-            placeholder: (context, url) => Container(
+            placeholder: (_, __) => Container(
               height: 130,
               width: screenWidth,
               color: Colors.grey[700],
             ),
-            errorWidget: (context, url, error) => Container(
+            errorWidget: (_, __, ___) => Container(
               height: 130,
               width: screenWidth,
               color: Colors.black,
@@ -78,80 +125,42 @@ class _ProfileInfoWidgetState extends State<ProfileInfoWidget> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.black, width: 3),
-                  ),
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage: user.profileImage.isNotEmpty
-                        ? CachedNetworkImageProvider(user.profileImage)
-                        : null,
-                    backgroundColor:
-                        user.profileImage.isEmpty ? Colors.grey : null,
-                    child: user.profileImage.isEmpty
-                        ? const Icon(Icons.person,
-                            size: 40, color: Colors.white)
-                        : null,
-                  ),
-                ),
-                const SizedBox(height: 12),
                 Row(
                   children: [
-                    Flexible(
-                      child: Row(
-                        children: [
-                          Flexible(
-                            child: RichText(
-                              text: TextSpan(
-                                style: const TextStyle(fontSize: 24),
-                                children: [
-                                  TextSpan(
-                                    text: user.name.isNotEmpty
-                                        ? user.name
-                                        : user.nip05.split('@').first,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  if (_isVerified == true &&
-                                      user.nip05.contains('@'))
-                                    const WidgetSpan(
-                                      alignment: PlaceholderAlignment.middle,
-                                      child: Padding(
-                                        padding:
-                                            EdgeInsets.symmetric(horizontal: 6),
-                                        child: Icon(
-                                          Icons.verified,
-                                          color: Color(0xFFECB200),
-                                          size: 20,
-                                        ),
-                                      ),
-                                    ),
-                                  if (user.nip05.isNotEmpty &&
-                                      user.nip05.contains('@'))
-                                    TextSpan(
-                                      text: '@${user.nip05.split('@').last}',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: const Color(0xFFECB200),
-                                        decoration: _isVerified == false
-                                            ? TextDecoration.lineThrough
-                                            : null,
-                                      ),
-                                    ),
-                                ],
+                    _buildAvatar(user),
+                    const Spacer(),
+                    if (_currentUserNpub != null &&
+                        widget.user.npub != _currentUserNpub &&
+                        _isFollowing != null)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 35.0),
+                        child: GestureDetector(
+                          onTap: _toggleFollow,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            height: 34,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.white10,
+                              borderRadius: BorderRadius.circular(24),
+                              border: Border.all(color: Colors.white30),
+                            ),
+                            child: Text(
+                              _isFollowing! ? 'Following' : 'Follow',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
                   ],
                 ),
                 const SizedBox(height: 12),
+                _buildNameRow(user),
+                const SizedBox(height: 6),
                 if (user.lud16.isNotEmpty)
                   Text(
                     user.lud16,
@@ -197,6 +206,70 @@ class _ProfileInfoWidgetState extends State<ProfileInfoWidget> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildAvatar(UserModel user) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.black, width: 3),
+      ),
+      child: CircleAvatar(
+        radius: 40,
+        backgroundImage: user.profileImage.isNotEmpty
+            ? CachedNetworkImageProvider(user.profileImage)
+            : null,
+        backgroundColor: user.profileImage.isEmpty ? Colors.grey : null,
+        child: user.profileImage.isEmpty
+            ? const Icon(Icons.person, size: 40, color: Colors.white)
+            : null,
+      ),
+    );
+  }
+
+  Widget _buildNameRow(UserModel user) {
+    return Row(
+      children: [
+        Flexible(
+          child: RichText(
+            text: TextSpan(
+              style: const TextStyle(fontSize: 24),
+              children: [
+                TextSpan(
+                  text: user.name.isNotEmpty
+                      ? user.name
+                      : user.nip05.split('@').first,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                if (_isVerified == true && user.nip05.contains('@'))
+                  const WidgetSpan(
+                    alignment: PlaceholderAlignment.middle,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 6),
+                      child: Icon(Icons.verified,
+                          color: Color(0xFFECB200), size: 20),
+                    ),
+                  ),
+                if (user.nip05.isNotEmpty && user.nip05.contains('@'))
+                  TextSpan(
+                    text: '@${user.nip05.split('@').last}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFFECB200),
+                      decoration: _isVerified == false
+                          ? TextDecoration.lineThrough
+                          : null,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
