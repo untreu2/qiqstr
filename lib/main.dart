67d44a31f06604b197be27dd5bc039b5502966bf
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-05-08 17:14:36 +0300

    Optimized app startup

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'models/notification_model.dart';
 import 'models/zap_model.dart';
 import 'screens/login_page.dart';
 import 'screens/feed_page.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +31,28 @@ Future<void> main() async {
 
     await Hive.openBox<LinkPreviewModel>('link_preview_cache');
 
-    runApp(const ProviderScope(child: QiqstrApp()));
+    final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+    String? privateKey = await secureStorage.read(key: 'privateKey');
+    String? npub = await secureStorage.read(key: 'npub');
+
+    if (privateKey != null && npub != null) {
+      await Hive.openBox<NoteModel>('notes_Feed_$npub');
+      await Hive.openBox<ReactionModel>('reactions_Feed_$npub');
+      await Hive.openBox<ReplyModel>('replies_Feed_$npub');
+      await Hive.openBox<RepostModel>('reposts_Feed_$npub');
+      await Hive.openBox<NoteModel>('notes_Profile_$npub');
+      await Hive.openBox<ReactionModel>('reactions_Profile_$npub');
+      await Hive.openBox<ReplyModel>('replies_Profile_$npub');
+      await Hive.openBox<RepostModel>('reposts_Profile_$npub');
+      await Hive.openBox<UserModel>('users');
+      await Hive.openBox<FollowingModel>('followingBox');
+      await Hive.openBox<NotificationModel>('notifications_$npub');
+      await Hive.openBox<ZapModel>('zaps_$npub');
+
+      runApp(ProviderScope(child: QiqstrApp(home: FeedPage(npub: npub))));
+    } else {
+      runApp(const ProviderScope(child: QiqstrApp(home: LoginPage())));
+    }
   } catch (e) {
     print('Error initializing Hive: $e');
     runApp(const HiveErrorApp());
@@ -40,7 +60,8 @@ Future<void> main() async {
 }
 
 class QiqstrApp extends ConsumerWidget {
-  const QiqstrApp({super.key});
+  final Widget home;
+  const QiqstrApp({super.key, required this.home});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -88,106 +109,7 @@ class QiqstrApp extends ConsumerWidget {
           textTheme: ButtonTextTheme.primary,
         ),
       ),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends ConsumerStatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  ConsumerState<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends ConsumerState<SplashScreen>
-    with TickerProviderStateMixin {
-  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-    );
-
-    _animation = TweenSequence([
-      TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.0), weight: 50),
-      TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 50),
-    ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-
-    _controller.forward().whenComplete(() => _checkStoredNsec());
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future<void> _checkStoredNsec() async {
-    try {
-      String? privateKey = await _secureStorage.read(key: 'privateKey');
-      String? npub = await _secureStorage.read(key: 'npub');
-
-      if (privateKey != null && npub != null) {
-        await Hive.openBox<NoteModel>('notes_Feed_$npub');
-        await Hive.openBox<ReactionModel>('reactions_Feed_$npub');
-        await Hive.openBox<ReplyModel>('replies_Feed_$npub');
-        await Hive.openBox<RepostModel>('reposts_Feed_$npub');
-        await Hive.openBox<NoteModel>('notes_Profile_$npub');
-        await Hive.openBox<ReactionModel>('reactions_Profile_$npub');
-        await Hive.openBox<ReplyModel>('replies_Profile_$npub');
-        await Hive.openBox<RepostModel>('reposts_Profile_$npub');
-
-        await Hive.openBox<UserModel>('users');
-        await Hive.openBox<FollowingModel>('followingBox');
-        await Hive.openBox<NotificationModel>('notifications_$npub');
-        await Hive.openBox<ZapModel>('zaps_$npub');
-
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => FeedPage(npub: npub),
-          ),
-        );
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const LoginPage(),
-          ),
-        );
-      }
-    } catch (e) {
-      print('Error reading secure storage or opening boxes: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("An error occurred while loading the app."),
-        ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: FadeTransition(
-        opacity: _animation,
-        child: Center(
-          child: SvgPicture.asset(
-            'assets/main_icon_white.svg',
-            width: 100,
-            height: 100,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      home: home,
     );
   }
 }
