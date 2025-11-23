a95bb34a166a1dc3441f73a913731a3015ba1c53
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-11-23 04:42:40 +0300

    Add network permissions for Android

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     afterEvaluate {
         if (project.hasProperty("android")) {
@@ -19,17 +20,9 @@ subprojects {
             if (androidExtension is com.android.build.gradle.BaseExtension) {
                 androidExtension.compileSdkVersion = "android-36"
                 androidExtension.buildToolsVersion = "36.0.0"
-                androidExtension.ndkVersion = "27.0.12077973"
-                if (androidExtension.namespace == null) {
-                    androidExtension.namespace = project.group.toString()
-                }
             }
         }
     }
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
